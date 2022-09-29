e9bed3362f5716882cdac6e1f432ac606cc15746
Ashishurfravi <ashishravi11@gmail.com>
2022-09-29 13:26:12 +0530

    code

--- Codefest-bot/bot.py
+++ Codefest-bot/bot.py
@@ -1,29 +1,85 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-from botbuilder.core import ActivityHandler, MessageFactory, TurnContext, StatePropertyAccessor
+from os import system
+import re
+from time import sleep
+from botbuilder.core import ActivityHandler, MessageFactory, TurnContext
 from botbuilder.schema import ChannelAccount, CardAction, ActionTypes, SuggestedActions
-from botbuilder.dialogs import Dialog
 
 
 class MyBot(ActivityHandler):
-    # See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+    """
+    This bot will respond to the user's input with suggested actions.
+    Suggested actions enable your bot to present buttons that the user
+    can tap to provide input.
+    """
+    async def on_members_added_activity(
+        self, members_added: ChannelAccount, turn_context: TurnContext
+    ):
+        """
+        Send a welcome message to the user and tell them what actions they may perform to use this bot
+        """
+        return await self._send_welcome_message(turn_context)
 
     async def on_message_activity(self, turn_context: TurnContext):
-        await turn_context.send_activity(f"{ turn_context.activity.text }!")
-        return await self._send_suggested_actions(turn_context)
+        """
+        Respond to the users choice and display the suggested actions again.
+        """
+        text = turn_context.activity.text
+        response_text = self._process_input(text)
+        try:
+            if "donate monthly" in response_text:
+                await turn_context.send_activity(MessageFactory.text(response_text))
+                return await self._donation_input(turn_context)
+            if response_text.startswith("Hi "):
+                await turn_context.send_activity(MessageFactory.text(response_text))
+                return await self._send_suggested_actions(turn_context)
+            await turn_context.send_activity(MessageFactory.text(response_text))
+            sleep(1) 
+        except TypeError as e:
+            await turn_context.send_activity("Thanks for contacting us, will reach out to you.")
+            sleep(1)        
+
+    async def _send_welcome_message(self, turn_context: TurnContext):
+        for member in turn_context.activity.members_added:
+            if member.id != turn_context.activity.recipient.id:
+                await turn_context.send_activity(
+                    MessageFactory.text(f"Welcome to Start Young UK !")
+                )
+                sleep(1)
+                return await turn_context.send_activity("May I know your name?")
+
+    def _process_input(self, text1: str):
+        if text1 == "Sponsor A Child":
+            sleep(1)
+            return f"Would you like to donate monthly or one time?"
+        if text1 == "Become a Campaigner":
+            sleep(1)
+            return f"Please enter your email id and we will get back to you"
+        if text1 == "Donate":
+            sleep(1)
+            return f"Would you like to donate monthly or one time?"
+        if text1 == "Become a Mentor":
+            sleep(1)
+            return f"Please enter your email id and we will get back to you"
+        sleep(1) 
+        if text1 != "Monthly" and text1 != "One Time" and ".com" not in text1:
+            return f"Hi {text1} !"
+        if "@" in text1 or ".com" in text1:
+            return f"Thanks for contacting us, will reach out to you."
+        if text1 != "Monthly" or text1 != "One Time":
+            sleep(1)
+            return f"Please enter your email id and we will get back to you"
         
-    async def on_members_added_activity(
-        self,
-        members_added: ChannelAccount,
-        turn_context: TurnContext
-    ):
-        for member_added in members_added:
-            if member_added.id != turn_context.activity.recipient.id:
-                await turn_context.send_activity("Welcome to Start Young UK !")
-                await turn_context.send_activity("May I know your name?")
 
     async def _send_suggested_actions(self, turn_context: TurnContext):
+        """
+        Creates and sends an activity with suggested actions to the user. When the user
+        clicks one of the buttons the text value from the "CardAction" will be displayed
+        in the channel just as if the user entered the text. There are multiple
+        "ActionTypes" that may be used for different situations.
+        """
         reply = MessageFactory.text("There are various ways to help and support us. Please select one of the below given options.")
         reply.suggested_actions = SuggestedActions(
             actions=[
@@ -33,9 +89,9 @@ class MyBot(ActivityHandler):
                     value="Sponsor A Child",
                 ),
                 CardAction(
-                    title="Become a Campaigner (Spread the word)",
+                    title="Become a Campaigner",
                     type=ActionTypes.im_back,
-                    value="Become a Campaigner (Spread the word)",
+                    value="Become a Campaigner",
                 ),
                 CardAction(
                     title="Donate",
@@ -46,7 +102,26 @@ class MyBot(ActivityHandler):
                     title="Become a Mentor",
                     type=ActionTypes.im_back,
                     value="Become a Mentor",
-                ),
+                )
             ]
         )
         await turn_context.send_activity(reply)
+
+    async def _donation_input(self, turn_context: TurnContext):
+        reply = MessageFactory.text(f"Select One")
+        reply.suggested_actions = SuggestedActions(
+            actions=[
+                CardAction(
+                    title="Monthly",
+                    type=ActionTypes.im_back,
+                    value="Monthly",
+                ),
+                CardAction(
+                    title="One Time",
+                    type=ActionTypes.im_back,
+                    value="One Time",
+                )
+            ]
+        )
+        #return await turn_context.send_activity(reply)
+        return await turn_context.send_activity(reply)
